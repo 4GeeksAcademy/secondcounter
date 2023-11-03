24d5aa9d0ee35b66fe78ba511a8327f45071be8e
Arturo <141342995+amancilla88@users.noreply.github.com>
2023-11-03 23:29:22 +0000

    counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function App() {
+  const [count, setCount] = useState(0);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const increment = () => {
+    setCount(count + 1);
+  };
 
-export default Home;
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const reset = () => {
+    setCount(0); // Reset to 0
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000); // Update the count every 1 second
+
+    return () => {
+      clearInterval(interval); // Cleanup the interval when the component unmounts
+    };
+  }, []);
+
+  return (
+    <div className="container my-5">
+      <div className="card text-center my-5">
+        <div className="card-body">
+          <h1>Counter App</h1>
+          <div className="my-5">
+            <h2>{count}</h2>
+            <button className="btn btn-success mx-3" onClick={increment}>
+              Increment
+            </button>
+            <button
+              className="btn btn-danger mx-3"
+              onClick={decrement}
+              disabled={count === 0}
+            >
+              Decrement
+            </button>
+            <button className="btn btn-secondary mx-3" onClick={reset}>
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
